49d7e5f8bcd283eb2c1b73d1d235b82d02332713
itskme <160423464+itskme@users.noreply.github.com>
2024-08-13 17:05:47 +0000

    HTML done + start JS

---
+++ script.js
@@ -0,0 +1,8 @@
+const apiKey = '//here i will put';
+const apiEndpoint = 'https://api.weather.gc.ca/collections/forecast-hourly';
+
+const weatherForm = document.getElementById('weather-form');
+const locationInput = document.getElementById('location');
+const submitButton = document.getElementById('submit');
+const weatherDataDiv = document.getElementById('weather-data');
+
